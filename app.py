ba9fb6ce8d83268ed02d7dc40c942ba1274fc70c
Team 4 <team4@futo.edu.ng>
2026-07-26 11:07:30 +0000

    Fix: move SQLite DB to writable /tmp for Render

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def create_app():
     app = Flask(__name__)
     basedir = os.path.abspath(os.path.dirname(__file__))
     app.config['SECRET_KEY'] = 'biz-analytics-key-2026'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'analytics.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('/tmp', 'analytics.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
